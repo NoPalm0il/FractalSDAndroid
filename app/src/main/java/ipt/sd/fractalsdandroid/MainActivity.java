d68e5a22e14928f820cbf7c6d76c1210f0a16b38
NoPalm0il <jpedrocr2000@hotmail.com>
2021-01-15 13:47:39 +0000

    sockets with object input, frames working

--- app/src/main/java/ipt/sd/fractalsdandroid/MainActivity.java
+++ app/src/main/java/ipt/sd/fractalsdandroid/MainActivity.java
@@ -2,30 +2,29 @@ package ipt.sd.fractalsdandroid;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Point;
 import android.graphics.PointF;
 import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String BAL_ADDRESS = "194.210.113.23:10011";
+
     ImageView fractalView;
-    EditText pointXTxt, pointYTxt, zoomTxt, iterationsTxt, serverAddressTxt, frames;
+    EditText pointXTxt, pointYTxt, zoomTxt, iterationsTxt, serverAddressTxt, framesTxt;
     Button generateFractalBt, resetValuesBt;
-    String[] serverAddr;
+    String[] serverAdr;
+    int frames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,19 +39,21 @@ public class MainActivity extends AppCompatActivity {
         serverAddressTxt = findViewById(R.id.serverAddressTxt);
         generateFractalBt = findViewById(R.id.generateFractalBt);
         resetValuesBt = findViewById(R.id.resetButton);
-        frames = findViewById(R.id.framesText);
+        framesTxt = findViewById(R.id.framesText);
+
+        serverAddressTxt.setText(BAL_ADDRESS);
 
         generateFractalBt.setOnClickListener((view) -> {
-            serverAddr = serverAddressTxt.getText().toString().split(":");
-            int frames = Integer.parseInt(String.valueOf(this.frames.getText()));
-            getFractalImage(serverAddr[0], Integer.parseInt(serverAddr[1]), getFractalParams(), frames);
+            serverAdr = serverAddressTxt.getText().toString().split(":");
+            frames = Integer.parseInt(String.valueOf(this.framesTxt.getText()));
+            getFractalFrames(serverAdr[0], Integer.parseInt(serverAdr[1]), getFractalParams(), frames);
         });
 
         resetValuesBt.setOnClickListener((view) -> {
             pointXTxt.setText("-1.7685374027917619472408834008472010565952062055525180562253268903921439965790");
             pointYTxt.setText("0.0005400495035209695647102872054426089298508762991118705637984149391509006042");
             zoomTxt.setText("4.0");
-            iterationsTxt.setText("500");
+            iterationsTxt.setText("1000");
 
             BitmapDrawable obj = new BitmapDrawable(getResources(), BitmapFactory.decodeByteArray(new byte[1], 0, 0));
             fractalView.setImageDrawable(obj);
@@ -68,9 +69,8 @@ public class MainActivity extends AppCompatActivity {
             pointXTxt.setText("" + newCenter.x);
             pointYTxt.setText("" + newCenter.y);
             double newZoom = Double.parseDouble(zoomTxt.getText().toString()) / 3.2;
-            zoomTxt.setText("" +newZoom);
-            serverAddr = serverAddressTxt.getText().toString().split(":");
-            // getFractalImage(serverAddr[0], Integer.parseInt(serverAddr[1]), getFractalParams());
+            zoomTxt.setText("" + newZoom);
+            serverAdr = serverAddressTxt.getText().toString().split(":");
         });
     }
 
@@ -83,44 +83,43 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private Bitmap readImage(DataInputStream input) throws IOException {
-        byte[] byteArray = new byte[1024 * 1024]; // 1 MB buffer
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-        int bytesRead, totalBytes = 0;
+    private ArrayList<Bitmap> readFrames(ObjectInputStream input) throws IOException, InterruptedException, ClassNotFoundException {
+        ArrayList<Bitmap> bitmaps = new ArrayList<>();
+        byte[][] byteArray; // 1 MB buffer
         //read all bytes from stream
-        while ((bytesRead = input.read(byteArray)) > 0) {
-            bos.write(byteArray, 0, bytesRead); // put byte array in memory
-            totalBytes += bytesRead; // increase total bytes
+        byteArray = (byte[][]) input.readObject();
+        for (byte[] frame : byteArray) {
+            Bitmap img = BitmapFactory.decodeByteArray(frame, 0, frame.length);
+            if (img != null)
+                bitmaps.add(img);
         }
         //builds a bitmap with byte array
-        return BitmapFactory.decodeByteArray(bos.toByteArray(), 0, totalBytes);
+        return bitmaps;
     }
 
-    public void getFractalImage(String serverName, int serverPort, String fractalParams, int frames) {
-        Thread thr = new Thread(() -> {
+    public void getFractalFrames(String serverName, int serverPort, String fractalParams, int frames) {
+        new Thread(() -> {
             try {
                 // open connection
                 Socket balc = new Socket(serverName, serverPort);
                 DataOutputStream out = new DataOutputStream(balc.getOutputStream());
-                DataInputStream in = new DataInputStream(balc.getInputStream());
+                ObjectInputStream in = new ObjectInputStream(balc.getInputStream());
                 //send fractal parameters to server
                 out.writeUTF(fractalParams);
                 out.flush();
-                // todo: acaba isto palhaço
-                //for (int i = 0; i < frames; i++) {
-                    // get fractal image
-                Bitmap bmp = readImage(in);
-                // display image
-                updateGui(bmp);
-                Thread.sleep(20);
-                //}
+                // get fractal image
+                ArrayList<Bitmap> bmp = readFrames(in);
+                 for (int i = 0; i < frames; i++) {
+                 // display image
+                 updateGui(bmp.get(i));
+                 Thread.sleep(40);
+                 }
                 // close connection
                 balc.close();
-            } catch (IOException | InterruptedException e) {
+            } catch (IOException | InterruptedException | ClassNotFoundException e) {
                 e.printStackTrace();
             }
-        });
-        thr.start();
+        }).start();
     }
 
     public PointF getRealCoordinates(float xx, float yy, int size, PointF center) {
@@ -133,12 +132,12 @@ public class MainActivity extends AppCompatActivity {
         return new PointF(x, y);
     }
 
-    private String getFractalParams(){
+    private String getFractalParams() {
         return pointXTxt.getText().toString() + " " +
                 pointYTxt.getText().toString() + " " +
                 zoomTxt.getText().toString() + " " +
                 iterationsTxt.getText().toString() + " " +
                 "500 500 " +
-                frames.getText().toString();
+                framesTxt.getText().toString();
     }
 }
